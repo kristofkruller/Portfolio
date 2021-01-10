557630fc32523e9002be655612b50654723109ff
kristofkruller <kristof.kruller@gmail.com>
2021-01-10 19:29:06 +0100

    testing email js

--- styles/app.js
+++ styles/app.js
@@ -10,3 +10,18 @@ hamburger.addEventListener("click", () => {
   });
 });
 /*Navbar circle effect*/
+/*Email function*/
+document.querySelector("textarea").addEventListener("submit".submitForm);
+
+function sendEmail(name, email, message) {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "design.kruller@gmail.com",
+    Password: "pqekqcthxbckwwsa",
+    To: "design.kruller@gmail.com",
+    From: "design.kruller@gmail.com",
+    Subject: `${name} sent you a review`,
+    Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
+  }).then(function (message) { alert("Mail sent successfully") 
+});
+}
